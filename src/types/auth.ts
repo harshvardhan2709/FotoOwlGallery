c2d5d723d312f34527717d2f75910dc91ed733dd
dark20004 <sawantharsh2022@gmail.com>
2026-08-11 23:11:27 +0530

    start

---
+++ src/types/auth.ts
@@ -0,0 +1,22 @@
+export type Gender = "Male" | "Female" | "Other";
+
+export interface User {
+  fullName: string;
+  email: string;
+  gender: Gender;
+  mobile: string;
+  address: string;
+  city: string;
+  password: string;
+}
+
+export interface AuthState {
+  user: User | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+
+  register: (user: User) => Promise<void>;
+  login: (email: string, password: string) => Promise<boolean>;
+  logout: () => Promise<void>;
+  loadSession: () => Promise<void>;
+}
